fa3d9442517c491d330d741b728634013f7e8681
spykelionel <social.lionel@gmail.com>
2024-04-02 16:26:28 +0100

    create a todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React from "react";
 
-export const Todo = ({task, handletoggleComplete, handletoggleComplete}) => {
-  
+/**
+ * A Todo component
+ * @param {*} task The task todo
+ * @param {*} handletoggleComplete Function to toggle todo completed state
+ *  @param {*} handleDeleteTask Function to delete a todo
+ * @returns JSX
+ */
+export const Todo = ({ task, handletoggleComplete, handleDeleteTask }) => {
   return (
-     <li
-                    key={task?.id}
-                    className={`list-group-item d-flex align-items-center ${
-                      task?.completed ? "completed" : ""
-                    }`}
-                  >
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      checked={task?.completed}
-                      onChange={() => handletoggleComplete(task?.id)}
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="flexCheckDefault"
-                    >
-                      {task?.text}
-                    </label>
-                    <a
-                      className="ms-auto"
-                      onClick={() => handleDeleteTask(task.id)}
-                    >
-                      <i className="bi bi-trash "></i>
-                    </a>
-                  </li>
-);
+    <li
+      key={task?.id}
+      className={`list-group-item d-flex align-items-center ${
+        task?.completed ? "completed" : ""
+      }`}
+    >
+      <input
+        className="form-check-input me-2"
+        type="checkbox"
+        checked={task?.completed}
+        onChange={() => handletoggleComplete(task?.id)}
+      />
+      <label className="form-check-label" htmlFor="flexCheckDefault">
+        {task?.text}
+      </label>
+      <a className="ms-auto" onClick={() => handleDeleteTask(task.id)}>
+        <i className="bi bi-trash "></i>
+      </a>
+    </li>
+  );
+};
